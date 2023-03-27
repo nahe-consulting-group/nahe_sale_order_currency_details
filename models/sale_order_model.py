4d4e6ffb2a4470c08a352e45ae924f1b9b3506f4
Nähe-cg <66278632+nahe-consulting-group@users.noreply.github.com>
2023-03-27 14:57:45 -0300

    Update sale_order_model.py

--- models/sale_order_model.py
+++ models/sale_order_model.py
@@ -3,9 +3,9 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    amount_total_company_currency = fields.Monetary(compute='_compute_amount_total_company_currency', readonly=True, currency_field='company_id.currency_id')
+    amount_total_company_currency = fields.Monetary(compute='_compute_amount_total_company_currency', readonly=True)
 
-    @api.depends('amount_total', 'pricelist_id.currency_id', 'company_id.currency_id')
+    @api.depends('amount_total', 'pricelist_id.currency_id')
     def _compute_amount_total_company_currency(self):
         for order in self:
             currency_rate = order.pricelist_id.currency_id.with_context(date=order.date_order).rate
@@ -14,9 +14,9 @@ class SaleOrder(models.Model):
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    amount_total_company_currency = fields.Monetary(compute='_compute_amount_total_company_currency', readonly=True, currency_field='company_id.currency_id')
+    amount_total_company_currency = fields.Monetary(compute='_compute_amount_total_company_currency', readonly=True)
 
-    @api.depends('amount_total', 'currency_id', 'company_id.currency_id')
+    @api.depends('amount_total', 'currency_id')
     def _compute_amount_total_company_currency(self):
         for move in self:
             currency_rate = move.currency_id.with_context(date=move.invoice_date or fields.Date.today()).rate
